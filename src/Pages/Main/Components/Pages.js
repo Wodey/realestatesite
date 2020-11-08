ff750602010b45d8f6727bb1b04c0553a98a3eed
Wodey <ivannewest@gmail.com>
2020-11-08 21:53:59 +0300

    + working pages component

--- src/Pages/Main/Components/Pages.js
+++ src/Pages/Main/Components/Pages.js
@@ -1,14 +1,69 @@
 import React from 'react';
 import styled from 'styled-components';
+import {useSelector, useDispatch} from "react-redux";
+import {nextPage, prevPage, setPage} from "../../../services/pagesReducer/actions.js";
+const OutlineWrapper = styled.div`
+  position: absolute;
+  bottom: 100px;
+  width: 20%;
+`;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const Button = styled.button`
+  outline: none;
+  border: none;
+  background: white;
+  cursor: pointer;
+  font-size: 1.3rem;
+  &:hover {
+    color: red;
+  }
+`;
+const Item = styled.span`
+  font-size: 1.3rem;
+  cursor: pointer;
+  color: ${props => props.activated ? "red" : "black"};
+`;
+const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const pages = [1, 2, 3, 4, 5, 6, 7, 8];
-const current = 0;
+
 export default () => {
+  const dispatch = useDispatch();
+  const current = useSelector(s => s.pages.current);
   return (
-    <Wrapper>
-      
-    </Wrapper>
+    <OutlineWrapper>
+      <Wrapper>
+        <Button onClick={() => dispatch(prevPage())} disabled={current === 0}>Prev</Button>
+        {
+          current > 3 ? <><Item onClick={() => dispatch(setPage(0))}>1</Item><Item>...</Item></> : ""
+        }
+        {
+          current === 3 ? <Item onClick={() => dispatch(setPage(0))}>1</Item> : ""
+        }
+        {
+          current > 1 ? <Item onClick={() => dispatch(setPage(current - 2))}>{pages[current-2]}</Item> : ""
+        }
+        {
+          current > 0 ? <Item onClick={() => dispatch(setPage(current - 1))}>{pages[current-1]}</Item> : ""
+        }
+          <Item activated={true}>{pages[current]}</Item>
+        {
+          current <= pages.length - 2 ? <Item onClick={() => dispatch(setPage(current + 1))}>{pages[current + 1]}</Item> : ""
+        }
+        {
+            current <= pages.length - 3 ? <Item onClick={() => dispatch(setPage(current + 2))}>{pages[current + 2]}</Item> : ""
+        }
+        {
+          current === pages.length - 4 ? <Item onClick={() => dispatch(setPage(pages.length - 1))}>{pages.length}</Item> : ""
+        }
+        {
+          current < pages.length - 4 ? <><Item>...</Item><Item onClick={() => dispatch(setPage(pages.length - 1))}>{pages.length}</Item></> : ""
+        }
+        <Button onClick={() => dispatch(nextPage())} disabled={current === pages.length - 1}>Next</Button>
+      </Wrapper>
+    </OutlineWrapper>
   )
 }
